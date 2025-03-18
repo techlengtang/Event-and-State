05716167fd79a6fb0245f4eeb1c13dd9b3fe9aad
techlengtang <techlengtang01@gmail.com>
2025-03-18 16:29:47 +0700

    Add files via upload

---
+++ Exercise 1/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./index.css"; // Ensure you have this CSS file with .sunny and .rainy styles
+
+function App() {
+  const [isRaining, setIsRaining] = useState(false);
+
+  function onSunClick() {
+    setIsRaining(false);
+  }
+
+  function onRainClick() {
+    setIsRaining(true);
+  }
+
+  function getTitle() {
+    return isRaining ? "It's Raning!" : "It's Sunny!";
+  }
+
+  function getBackgroundColor() {
+    return isRaining ? "rainy" : "sunny";
+  }
+
+  return (
+    <main className={getBackgroundColor()}>
+      <h1>{getTitle()}</h1>
+      <button onClick={onSunClick}>Sunny Time</button>
+      <button onClick={onRainClick}>Rain Time</button>
+    </main>
+  );
+}
+
+export default App;
